c7f2080bb86c7562ddac5e136e5605065de33954
Mindy <Mindy@192.168.229.1>
2014-04-19 19:55:35 +0800

    w8-01

--- W8-01_STLIntro/src/W8-01_STLIntro.cpp
+++ W8-01_STLIntro/src/W8-01_STLIntro.cpp
@@ -22,5 +22,16 @@ int main() {
 		cout << *i << ",";
 	cout << endl;
 
+	vector<int>::reverse_iterator r; //反向迭代器
+	for (r = v.rbegin(); r != v.rend(); r++)
+		cout << *r << ",";
+	cout << endl;
+
+	vector<int>::iterator j; //非常量迭代器
+	for (j = v.begin(); j != v.end(); j++)
+		*j = 100;
+	for (i = v.begin(); i != v.end(); i++)
+		cout << *i << ",";
+
 	return 0;
 }
